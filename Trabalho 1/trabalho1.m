531cb7e785da902be4a3dca32ef097936b30e552
ThiiD <thiago.saber@engenharia.ufjf.br>
2022-05-17 23:49:40 -0300

    Programa funcionando!

--- Trabalho 1/trabalho1.m
+++ Trabalho 1/trabalho1.m
@@ -1,5 +1,47 @@
-t = linspace(0,2*pi,1000)
-p_x = 20*cos(t)
-p_y = 20.*sin(t).*cos(t)
+clc; clear all; close all
 
-plot(p_x, p_y)
+% Posicao de leitura do sensor
+s = [20
+     0];
+ 
+% Posicao real do robo
+p = [20
+     0];
+ 
+% Ruidos de medicao
+eP = [0
+      0];
+
+% Tempo maximo
+tmax = 2 * pi;
+
+% Variação de tempo (passo)
+dt = pi/150;
+
+% Loop para plot do grafico
+for t=0:dt:tmax
+    % Nova posicao do robo
+    dP = [20*cos(t)
+          20*sin(t).*cos(t)];
+    
+    % Erro de leitura (ruido)
+    eP = randn(2,1);
+    
+    %Concatenacao dos valores antigos com o novo valor de posicao
+    p = [p , dP];
+    
+    % Concatenacao dos valores antigos com erro de medicao do robo com a 
+    % nova medicao com erro de posicao
+    s = [s , dP+eP];
+    
+    % Plot animado da posicao e da posicao medida
+    plot(p(1,:), p(2,:), 'k', 'linewidth', 2);
+    hold on
+    plot(s(1,:), s(2,:), 'r', 'linewidth', 2);
+    grid on, axis equal
+    hold off
+    xlabel("X")
+    ylabel("Y")
+    legend('p(t) - posição do robô','s(t) - leitura do sensor GPS')
+    drawnow
+end
